41ec2ec32b7967f3b98f988a23b8011374b31e2a
jeanmmlima <jean.lima@imd.ufrn.br>
2026-03-30 13:23:32 -0300

    first example - native res

---
+++ NativeRes/NativeRes/NativeResApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeResApp.swift
+//  NativeRes
+//
+//  Created by Jean Mário Moreira de Lima on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct NativeResApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
